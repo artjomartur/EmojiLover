a9884205fe50fe64dfc4642c2f46b108a53dc5b9
Artjom Becker <artjomb.2001@gmail.com>
2023-08-13 16:24:25 +0200

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Artjom Becker on 13.08.23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
