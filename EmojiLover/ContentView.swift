9c36975ad253c024f40e43d2773deef57f08ab97
Artjom Becker <artjomb.2001@gmail.com>
2023-08-13 16:26:10 +0200

    first commit, basic app with slider for 4 diffrent emojis for showcase

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,17 +7,30 @@
 
 import SwiftUI
 
+
+enum Emoji:String, CaseIterable {
+    case  😱, 🤘🏻, 🦅, 😇
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .😇
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue).font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self) { emoji in Text(emoji.rawValue)}
+                }
+            }
+            .navigationTitle("Emoji Lover!")
+            .pickerStyle(.segmented).padding()
         }
-        .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
